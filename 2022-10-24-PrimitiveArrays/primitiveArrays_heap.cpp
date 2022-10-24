f8b60219ead107601e63e0923a9fd84876905336
Iluvatar <woquendo@gmail.com>
2022-10-24 11:41:10 -0500

    New and delete , heap

---
+++ 2022-10-24-PrimitiveArrays/primitiveArrays_heap.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int main(int arg, char **argv) {
+
+  double a = 10.9;
+
+  double * data;
+  data = new double [20]; // ask for memory
+
+  double b = 11.987;
+
+  std::cout << "data[0] = " << data[0] << std::endl;
+  std::cout << "&data[0] = " << &data[0] << std::endl;
+  std::cout << "&data[1] = " << &data[1] << std::endl;
+  std::cout << "data = " << data << std::endl; // this is the same as &data[0]
+  std::cout << "data[20] = " << data[20] << std::endl; // ERROR copilot
+  std::cout << "data[-1] = " << data[-1] << std::endl; // ERROR
+
+  delete [] data; // free memory
+
+  return 0;
+}
+
+// data[5] -> data + 5 * sizeof(double)
+// data[20] -> data + 20 * sizeof(double)
+// data[-1] -> data - 1 * sizeof(double)
